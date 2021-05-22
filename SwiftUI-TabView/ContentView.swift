2f89dd4e0df5ad100348006f6854ec1e3a52c5a5
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-05-22 15:03:31 +0900

    Initial Commit

---
+++ SwiftUI-TabView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-TabView
+//
+//  Created by 萩倉丈 on 2021/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-TabView/SwiftUI_TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabViewApp.swift
+//  SwiftUI-TabView
+//
+//  Created by 萩倉丈 on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
