7403ea9d60d9a730219697f7f7a088e02f1b5c3c
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-05-22 15:24:29 +0900

    タブ追加

--- SwiftUI-TabView/ContentView.swift
+++ SwiftUI-TabView/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedTag = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selectedTag) {
+            VStack {
+                Spacer()
+                Text("萩倉")
+                Spacer()
+                HStack{
+                    Text("年齢")
+                    Text("XXさい")
+                }
+                HStack{
+                    Text("所属")
+                    Text("関西学院")
+                }
+            }.tabItem {
+                Text("萩倉タブ")
+            }.tag(1)
+            Text("福井")
+                .tabItem {
+                    Text("福井タブ")
+                }.tag(2)
+        }
     }
 }
 
